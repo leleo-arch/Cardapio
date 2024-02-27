bbe6f7f9915eabdf57dcba83ba421dfc6b096487
leleo-arch <leo_xdplay@hotmail.com>
2024-02-27 13:11:10 -0300

    Projéto - Cardápio - Js

---
+++ Cardapio/Prdocuts.js
@@ -0,0 +1,11 @@
+const menuOptions = [
+    { name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpeg' },
+    { name: 'X-Bacon', price: 34, vegan: false, src: './img/xbacon.png' },
+    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/bacon-egg.png' },
+    { name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png' },
+    { name: 'BigVegano', price: 55, vegan: true, src: './img/xvegan.png' },
+    { name: 'X-Vegan', price: 45, vegan: true, src: './img/monstruoso-veegan.png' },
+    
+]
+
+///e que a lista está sendo atualizada corretamente no documento HTML.///
---
+++ Cardapio/scripts.js
@@ -0,0 +1,58 @@
+
+const list = document.querySelector("ul")
+const mapeando = document.querySelector(".Mapear")
+const mostrartudo = document.querySelector(".mostrartudo")
+const somartudo = document.querySelector(".somar-tudo")
+const filtro = document.querySelector(".filtrar-tudo")
+
+function showall(productsArray) {
+    let Myli = '';
+    productsArray = menuOptions.forEach(product => {
+        Myli += `
+         <li>
+            <img src= ${product.src}> 
+            <p>${product.name}> </p>
+            <p class = "priceitem" >$ ${product.price} </p>
+         </li>
+         `;
+    });
+
+    list.innerHTML = Myli;
+}
+
+function descont() {
+    const descontando = menuOptions.map(prices => {
+        prices.price = prices.price * 0.9
+        return prices;
+
+    })
+
+    showall(descontando)
+}
+ 
+function somando () {
+const soma = menuOptions.reduce((acumulador, valor) => {
+    if (valor.price > 0) {
+        return acumulador + valor.price;
+    } else {
+        return acumulador;
+    }
+}, 0);
+
+console.log (soma)
+list.textContent = soma;
+
+}
+
+function filtros() {
+    const filtrando = menuOptions.filter((valor) => valor.vegan === true);
+    showall(filtrando);
+    console.log(filtrando);
+  
+  }
+
+
+mostrartudo.addEventListener('click', () => showall(menuOptions));
+filtro.addEventListener('click', filtros)
+mapeando.addEventListener('click', descont)
+somartudo.addEventListener('click', somando)
